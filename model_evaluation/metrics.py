2abb43f55bbdf8c10b296aad277e9db296c9a9ac
NathanRxl <nathan.rouxel@gmail.com>
2017-02-13 14:08:38 +0100

    Change the format of argument in score function
    In metrics.mean_average_precision, the arguments were first a list
    of lists. We decided to change the format of prediction to dict of
    lists for convenience.
    
    This change adapts metrics.mean_average_precision,
    in order to be able to compute a score from the output of model.predict
    without any parsing

--- model_evaluation/metrics.py
+++ model_evaluation/metrics.py
@@ -1,40 +1,36 @@
 import numpy as np
-import warnings
 
 
 def average_precision(recipients_pred, true_recipients):
     """
-    Compute average precision@10 between 2 lists (one recipients prediction for one message)
+    Compute average precision@10 between 2 lists
+    (one recipients prediction for one message)
     """
     precision = 0.0
     true_recipients_in_predictions = 0
 
     for prediction_rank, prediction in enumerate(recipients_pred):
-        if prediction in true_recipients and prediction not in recipients_pred[:prediction_rank]:
+        if (prediction in true_recipients and
+                prediction not in recipients_pred[:prediction_rank]):
             true_recipients_in_predictions += 1
             precision += true_recipients_in_predictions / (prediction_rank + 1)
 
     return round(precision / min(len(true_recipients), 10), 5)
 
 
-def mean_average_precision(recipients_preds, true_recipients_list):
+def mean_average_precision(mids_prediction, mids_true_recipients):
     """
-    Compute *mean* average precision@10 between 2 lists of lists
+    Compute *mean* average precision@10 between 2 dictionaries of predictions
     (all recipients prediction for all messages)
+    The dictionaries are assumed to have mids as keys and a list of prediction
+    as values.
     """
-    if all(isinstance(recipients_pred, list) for recipients_pred in recipients_preds):
-        each_prediction_average_precision = list()
 
-        for recipients_pred, true_recipients in zip(recipients_preds, true_recipients_list):
-            each_prediction_average_precision.append(
-                average_precision(recipients_pred, true_recipients)
-            )
+    each_prediction_average_precision = list()
 
-        return np.mean(each_prediction_average_precision)
-
-    else:
-        warnings.warn(
-            "Use average precision to compute an average precision@10 between two lists",
-            UserWarning
+    for mid, prediction in mids_prediction.items():
+        each_prediction_average_precision.append(
+            average_precision(prediction, mids_true_recipients[mid])
         )
-        return average_precision(recipients_preds, true_recipients_list)
+
+    return np.mean(each_prediction_average_precision)
